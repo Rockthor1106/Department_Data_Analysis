f64d4d4fdfde7346f32f0fc089b2b184ec9b4493
Jhan Carlos Carvajal Bastidas <jan.carvajal1@u.icesi.edu.co>
2022-01-29 17:59:54 -0500

    Add project files.

---
+++ Department_Data_Analysis/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using Microsoft.Win32;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data;
+using System.Collections;
+
+namespace Department_Data_Analysis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileButton_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.ShowDialog();
+
+            Student student = new Student();
+            string[] StudentArray = new string[3];
+
+            DataTable dt = new DataTable();
+            dt.Columns.Add("Last Name", typeof(string));
+            dt.Columns.Add("First Name", typeof(string));
+            dt.Columns.Add("Subject", typeof(string));
+
+            using (StreamReader sr = new StreamReader(openFileDialog.FileName))
+            {
+                while(!sr.EndOfStream)
+                {
+                    StudentArray = sr.ReadLine().Split(";");
+                    txtBox1.Text = sr.Readli
+                }
+            }
+        }
+    }
+}
---
+++ Department_Data_Analysis/model/Department.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Department_Data_Analysis.model
+{
+    class Department
+    {
+        public string Code { get; set; }
+        public List<int> MunicipalitiesCode { get; set; }
+        public string Name { get; set; }
+        public List<string> Municipalities { get; set; }
+        public List<string> MunicipalitiesType { get; set; }
+
+        public Department(string Code, List<int> MunicipalitiesCode, string Name, List<string> Municipalities, List<string> MunicipalitiesType) 
+        {
+            this.Code = Code;
+            this.MunicipalitiesCode = MunicipalitiesCode;
+            this.Name = Name;
+            this.Municipalities = Municipalities;
+            this.MunicipalitiesType = MunicipalitiesType;
+        }
+    }
+}
